779381c2f2cc46d7ca952a554e751b3b0a298033
Leo Pragash Lazarus <leonerdo037@gmail.com>
2018-12-27 12:40:16 +0000

    Initial Commit

---
+++ Source/AzureHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class AzureHandler
+{
+    private AzureCredentials credentials;
+    private IAzure azure;
+
+
+}
---
+++ Source/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ARM
+{
+    class Program
+    {
+        static string curPath = Environment.CurrentDirectory + @"\etc\network\";
+        static string configFile = curPath + @"\ARM.config";
+
+        static void Main(string[] args)
+        {
+            Banner();
+            if (!File.Exists(configFile))
+            {
+                Setup();
+            }
+        }
+
+        static void Banner()
+        {
+            Console.Clear();
+            Console.WriteLine("+-+-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+");
+            Console.WriteLine("|A|Z|U|R|E| |R|E|P|O|R|T| |M|A|N|A|G|E|R|");
+            Console.WriteLine("+-+-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+");
+            Console.WriteLine("Version 0.1" + Environment.NewLine);
+        }
+
+        static void Setup()
+        {
+            Banner();
+
+        }
+    }
+}
